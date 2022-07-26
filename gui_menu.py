c14ecfc816b276a91b8918ea201ca33158556657
kjptts2241 <kjptts2241@gmail.com>
2022-07-26 13:59:20 +0900

    AI 메뉴 추천 GUI 버전 개발

--- gui_menu.py
+++ gui_menu.py
@@ -1,43 +1,42 @@
 from tkinter import *
+from PIL import ImageTk,Image
 import random
 
 #  예제 1) tkinter 파이썬 GUI 레이블(label)
-# tkinter를 사용하여 텍스트를 나타내보자
+# tkinter를 사용하여 텍스트와 이미지를 나타내보자
 
 menulist = ['짜장면', '짬뽕', '탕수육', '라면', '국밥']
 
-# 1. 루트화면 (root window) 생성
+# 루트화면 (root window) 생성
 tk = Tk()
 
-# 2. 텍스트 표시
-label = Label(tk,text='AI가 추천 해주는 메뉴는?') 
+# window 창 크기 설정
+tk.geometry('800x600')
 
-# 3. 레이블 배치 실행
-label.pack()
+# gui 타이틀
+tk.title("오늘 머 먹지?")
+
+# 텍스트 표시
+label = Label(tk, text='AI가 추천 해주는 메뉴는?', font=("돋음", 10))
+
+# 레이블 배치 실행
+label.pack(side = TOP, padx = 20, pady = 20)
 
 # 함수 정의 (버튼을 누르면 텍스트 내용이 바뀜)
 def event():
     menu = random.choice(menulist)
-    button['text'] = f'{menu} 추천!'
-    # menuimage()
+    msg = '{} 추천!'.format(menu)
+    label.configure(text=msg)
+    img = PhotoImage(file = '{}.png'.format(menu)) # 이미지 읽고
+    label_image = Label(image = img) # 이미지 넣고
+    label_image.image = img # 레퍼런스 추가
+    # label_image.grid(column = 0, row = 2)
+    label_image.pack(side = TOP, padx = 10, pady = 10)
 
-def imagechange():
-    if button['text'] == '짬뽕':
-        menuimage()
-    if button['text'] == '짜장면':
-        menuimage2()
-
-button = Button(tk, text='추천 메뉴',command=event)
+button = Button(tk, text='추천 메뉴', command=event)
 # button2 = Button(tk, text='버튼2 입니다.')
-button.pack(side=LEFT, padx=10, pady=10) #side로 배치설정, padx로 좌우 여백설정, pady로 상하 여백설정 
+button.pack(side = BOTTOM, padx = 10, pady = 10) #side로 배치설정, padx로 좌우 여백설정, pady로 상하 여백설정 
 # button2.pack(side=LEFT, padx=10, pady= 10)
-
-image = PhotoImage(file = '짬뽕.png') #경로를 따로 지정하지 않으면 파일이 있는 위치를 가리킴.
-image2 = PhotoImage(file = '짜장면.png')
-menuimage = Label(tk, image=image)
-menuimage2 = Label(tk, image=image2)
-menuimage.pack()
-menuimage2.pack()
 
 # 4. 메인루프 실행
 tk.mainloop()
